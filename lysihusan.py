852d26603b8da1718dc0596703b3ebc2feb0deb5
Håkon Klausen <hakon.klausen@ae.no>
2022-10-05 13:25:14 +0200

    Added more startup text Excluded dist and build

--- lysihusan.py
+++ lysihusan.py
@@ -3,9 +3,11 @@ import time
 import pyautogui 
 
 y_movement = 10
-print("Lys i Husan started, pressing F15 every second...  Press Ctrl-C to end")
+print("Lys i Husan v1.0 started, pressing F15 every minute.  Press Ctrl-C to end")
+print("Released under the GNU General Public License v2.0 on GitHub: agderenergi/lysihusan/LICENSE")
+print("For info contact hakon.klausen@ae.no")
 while True:
 	#pyautogui.moveRel(0, y_movement)
 	#y_movement = y_movement * -1
 	pyautogui.press('f15')
-	time.sleep(1)
+	time.sleep(60)
