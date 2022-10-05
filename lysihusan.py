80b08c91b0c3ad0d467e44d4ea0287f747026926
Håkon Klausen <Hakon.Klausen@ae.no>
2022-10-05 10:07:48 +0200

    Added startup message

--- lysihusan.py
+++ lysihusan.py
@@ -3,6 +3,7 @@ import time
 import pyautogui 
 
 y_movement = 10
+print("Lys i Husan started, pressing F15 every second...  Press Ctrl-C to end")
 while True:
 	#pyautogui.moveRel(0, y_movement)
 	#y_movement = y_movement * -1
