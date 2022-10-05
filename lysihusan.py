bfe9d47352499e923d9e8a7304fcfee14b2512b4
Håkon Klausen <Hakon.Klausen@ae.no>
2022-10-05 14:53:20 +0200

    Added basic exception handling

--- lysihusan.py
+++ lysihusan.py
@@ -2,12 +2,13 @@ import time
 
 import pyautogui 
 
-y_movement = 10
-print("Lys i Husan v1.0 started, pressing F15 every minute.  Press Ctrl-C to end")
+pyautogui.FAILSAFE = True
+print("Lys i Husan v1.1 started, Pressing F15 every minute.  Close window to end")
 print("Released under the GNU General Public License v2.0 on GitHub: agderenergi/lysihusan/LICENSE")
 print("For info contact hakon.klausen@ae.no")
 while True:
-	#pyautogui.moveRel(0, y_movement)
-	#y_movement = y_movement * -1
-	pyautogui.press('f15')
-	time.sleep(60)
+	try:
+		pyautogui.press('f15')
+		time.sleep(59)
+	except:
+		continue
